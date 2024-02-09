d53fa07ab7188ab0f8622abc123a122657db5bef
Monark Vekariya <monarkvekariya@gmail.com>
2024-02-09 22:45:39 +0530

    final

---
+++ src/components/Logo.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+import {motion} from "framer-motion"
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div classname='flex items-center justify-center mt-2'>
+        <MotionLink href="/"
+        className='w-16 h-16 bg-dark text-light flex item-center justify-center 
+        rounded-full text-2xl font-bold'
+        whileHover={{scale:1.5}}
+        >VM</MotionLink>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import Logo from '@/components/Logo'
+import Link from 'next/link'
+import React from 'react'
+
+
+
+const NavBar = () => {
+  return (
+    <header 
+    classname='w-full px-32 py-8 font-large flex item-center justify-between'
+    >
+        <nav>
+            <Link href="/">Home</Link>
+            <Link href="about">About</Link>
+            <Link href="projects">Projects</Link>
+            <Link href="articles">Articles</Link>
+        </nav>
+        <Logo/>
+        <nav>
+            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>T</Link>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
