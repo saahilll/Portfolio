9a6b6720a4e98f954f3626eac61a023cbeb4460e
Saahil <saahilpandya824@gmail.com>
2024-02-10 12:46:08 +0530

    add nav bar components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,49 @@
-import Logo from '@/components/Logo'
 import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/router'; 
+import TwitterIcon from './Icons';
 
-
+const CustomLink = ({href, title, className = ""}) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className ={`${className} relative group`}>
+    {title}
+    <span className = {`
+    h-[1px] inline-block bg-dark
+    absolute left-0 -bottom-0.5
+    group-hover:w-full transition-[width] ease duration-300
+    ${router.asPath === href? 'w-full': 'w-0'}
+    `}>&nbsp;</span>
+    </Link>
+  )
+}
 
 const NavBar = () => {
   return (
     <header 
-    className='w-full px-32 py-8 font-large flex item-center justify-between'
+    className='w-full px-32 py-8 font-medium flex item-center justify-between'
     >
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="about">About</Link>
-            <Link href="projects">Projects</Link>
-            <Link href="articles">Articles</Link>
+            <CustomLink href="/" title="Home" className='mr-4'/>
+            <CustomLink href="about" title="About" className='mx-4'/>
+            <CustomLink href="projects" title="Projects" className='ml-4'/>
+
         </nav>
-        <Logo/>
+
         <nav>
-            <Link href="/" taget={"_blank"}>T</Link>
-            <Link href="/" taget={"_blank"}>T</Link>
-            <Link href="/" taget={"_blank"}>T</Link>
-            <Link href="/" taget={"_blank"}>T</Link>
-            <Link href="/" taget={"_blank"}>T</Link>
-            <Link href="/" taget={"_blank"}>T</Link>
+            <Link href="/" taget={"_blank"}>
+              <TwitterIcon />
+            </Link>
+            <Link href="/" taget={"_blank"}>
+              T
+            </Link>
+            <Link href="/" taget={"_blank"}>
+              T
+            </Link>
         </nav>
+        <div className = "absolute left-[50%] top-2 translate-x-[-50%]">
+
+        </div>
     </header>
   )
 }
