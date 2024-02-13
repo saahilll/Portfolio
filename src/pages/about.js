2d80fe8fe7241d2305f19b947088a31e698d96df
Monark Vekariya <monarkvekariya@gmail.com>
2024-02-13 14:29:45 +0530

    latest

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,9 +17,10 @@ function about() {
     
     <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
-        <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
-        <div className='grid w-full grid-cols-8 gap-16'>
-            <div className='col-span-3 flex flex-col items-start justify-start'>
+        <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8'/>
+        <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+            <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8
+            '>
             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
             <p className='font-medium'>
             Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
@@ -37,7 +38,7 @@ every project I work on. I look forward to the opportunity to bring my skills an
             </p>
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark 
-            bg-light p-8 dark:bg-dark dark:border-light
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8
             '
             >
                 <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-light'/>
